e6aa4e10612bb316196d225fe29495dfe66c2fe4
zzz6687 <agirova94@gmail.com>
2025-09-19 21:21:25 -0700

    Worked on the right side of the website

--- src/app/right-container/right-container.component.ts
+++ src/app/right-container/right-container.component.ts
@@ -4,8 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-right-container',
   imports: [],
   templateUrl: './right-container.component.html',
-  styleUrl: './right-container.component.css'
+  styleUrl: './right-container.component.css',
 })
 export class RightContainerComponent {
+  today: boolean = false;
+  week: boolean = true;
 
+  celsius: boolean = true;
+  fahrenheit: boolean = false;
+
+  onTodayClick() {
+    this.today = true;
+    this.week = false;
+  }
+
+  onWeekClick() {
+    this.week = true;
+    this.today = false;
+  }
+
+  onCelsiusClick() {
+    this.celsius = true;
+    this.fahrenheit = false;
+  }
+
+  onFahrenheitClick() {
+    this.celsius = false;
+    this.fahrenheit = true;
+  }
 }
